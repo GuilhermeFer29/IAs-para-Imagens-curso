98f68f26cb1a34540612576a87d42208ca07e28b
GuilhermeFer29 <leciguilherme@gmail.com>
2024-12-16 20:34:13 -0300

    cat_and_dog
    Gerador de classificação apartir de modelo base de caes e gatos , com loop e amostra das imagens.

--- cat_dog.py
+++ cat_dog.py
@@ -1,4 +1,26 @@
 from datasets import load_dataset
+from IPython.display import display
+from transformers import pipeline
 
-database = load_dataset('cats_vs_dogs', split='train', streaming=True)
 
+# Carrega o dataset Cat_and_Dog
+database = load_dataset('Bingsu/Cat_and_Dog', split='test')
+
+# Variavel line recebe 10 imagens aleatorias
+line = database.shuffle()[:10]
+model = 'akahana/vit-base-cats-vs-dogs'
+  
+# Função definer o tamanho da imagem
+def show_image(image):
+    image = image.copy()
+    image.thubnail((250, 250)) # Tamanho da imagem
+    display(image)
+
+    
+classifier = pipeline(   'image-classification', model=model)
+
+# Imprime as imagens
+for image in line['image']:  
+    result = classifier(image)
+    print(result)  
+    show_image(image)
